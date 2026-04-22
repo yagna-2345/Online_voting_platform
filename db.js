a1bbeb4720a532fdcf49d740b11e26f68149aab8
yagna-2345 <vu.241fa04450@gmail.com>
2026-04-22 20:34:09 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Yagna@2006",
-  database: "voting_system"
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 db.connect(err => {
